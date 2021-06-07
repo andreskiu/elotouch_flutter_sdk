dd605d28f5ab4e777b7340e4987fffbe9eaaf8ed
S683989 <andres.vara@evertecinc.com>
2021-06-07 12:27:12 -0300

    added new method to platform channel.  Is necessary to import sdk from elotouch yet

--- android/src/main/java/com/evertec/elotouch/elotouch_flutter_plugin/ElotouchFlutterPlugin.java
+++ android/src/main/java/com/evertec/elotouch/elotouch_flutter_plugin/ElotouchFlutterPlugin.java
@@ -1,5 +1,8 @@
 package com.evertec.elotouch.elotouch_flutter_plugin;
 
+import android.content.Context;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -16,17 +19,49 @@ public class ElotouchFlutterPlugin implements FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private Context applicationContext;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    applicationContext = flutterPluginBinding.getApplicationContext();
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "elotouch_flutter_plugin");
     channel.setMethodCallHandler(this);
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    System.out.println("LLAMADA RECIBIDA!!!");
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else if (call.method.equals("print")) {
+
+      try {
+        if(applicationContext == null){
+          Log.e("CONTEXT NULO?", "SI");
+        }else{
+          Log.e("CONTEXT NULO?", "NO");
+
+          DeviceManager manager = DeviceManager.getInstance(EloPlatform.PAYPOINT_REFRESH,applicationContext);
+          //manager.getPrinter().
+          //manager.getPrinter()
+          Log.e("CONTEXT NULO?", "MANAGER CONSEGUIDO? ");
+
+          Printer printer = manager.getPrinter();
+
+          if(printer == null){
+
+            Log.e("Status","IMPRESORA NO ENCONTRADA");
+          }else{
+            Log.e("Status","CREAMOS LA IMPRESORA");
+          }
+
+          // Log.e("haspaper? " , hasPaper? "true":"false");
+        }
+
+      } catch (UnsupportedEloPlatform unsupportedEloPlatform) {
+        unsupportedEloPlatform.printStackTrace();
+      }
+      result.success("Ok, vamos a imprimir: " + call.arguments().toString());
     } else {
       result.notImplemented();
     }
--- lib/elotouch_flutter_plugin.dart
+++ lib/elotouch_flutter_plugin.dart
@@ -1,6 +1,6 @@
-
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class ElotouchFlutterPlugin {
@@ -10,5 +10,18 @@ class ElotouchFlutterPlugin {
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> printString({@required dynamic args}) async {
+    print("POR EJECUTAR EL METHOD PRINT");
+    try {
+      final String version = await _channel.invokeMethod('print', args);
+      print("version: " + version);
+      return version;
+    } on Exception catch (e) {
+      print("EXECUTION FAILS");
+      print(e);
+    }
+    return null;
   }
 }
